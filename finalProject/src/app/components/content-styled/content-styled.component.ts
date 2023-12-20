9f2ff6c58695f0c783b143caebd0abb16f18521f
code-jude <judeprashanth@outlook.com>
2023-12-20 15:17:25 -0800

    Styling Workouts Page

--- finalProject/src/app/components/content-styled/content-styled.component.ts
+++ finalProject/src/app/components/content-styled/content-styled.component.ts
@@ -10,16 +10,30 @@ export class ContentStyledComponent implements OnInit {
   ngOnInit(): void {
     this.startCountDownTierOneTwo();
     this.startCountDownTierThree();
+    let tierName: string | null = localStorage.getItem('tier');
+    if (tierName == "Just Looking") {
+    tierName = 'Motivated & All In'
+    }
+    // Try to do this where the latest year updates automatically
+
+    // const yearSpan = document.getElementById('#currentYear');
+    // const currentYear = new Date();
+    // yearSpan!.innerText = currentYear.getFullYear();
   }
+
+  // Conditionally activate these
 
   tierOne: boolean = true;
   tierOneTwo: boolean = true;
   tierTwoThree: boolean = true;
   tierThree: boolean = true;
+  showLiveVideo: boolean = false;
+  tierName: string = "";
+
+  // Add logic to only show one timer based on tier
 
   testCards: number[] = [1, 2, 3, 4, 5, 6, 7]
-  testCardsTwo: number[] = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,23,24,25]
-
+  testCardsTwo: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 43, 44, 45, 46, 47, 48, 49, 50]
 
   startCountDownTierOneTwo() {
     var countDownDate = new Date("Jan 7, 2024 15:37:25").getTime();
@@ -56,7 +70,7 @@ export class ContentStyledComponent implements OnInit {
     var countDownDate = new Date("Jan 7, 2024 15:37:25").getTime();
 
     // Update the count down every 1 second
-    var x = setInterval(function () {
+    var x = setInterval(() => {
 
       // Get today's date and time
       var now = new Date().getTime();
@@ -78,16 +92,19 @@ export class ContentStyledComponent implements OnInit {
       if (distance < 0) {
         clearInterval(x);
         document.getElementById("timerTierThree")!.innerHTML = "EXPIRED";
-        // ngIf to open up a card for live video here
+        this.showLiveVideo = true;
       }
     }, 1000);
   }
 
   // Condition to display various items on page
   // localStorage.getItem('tier') === 'Just Looking';
- 
+
 
 
 }
+
+
+
 
 
