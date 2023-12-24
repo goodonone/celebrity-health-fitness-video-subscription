862b64649a2005bdb6f35915e95d186bc8212636
Gerrit <gerrit.spatz@hotmail.de>
2023-12-23 19:41:22 -0800

    basic store implementation

--- finalProject/src/app/components/store/store.component.ts
+++ finalProject/src/app/components/store/store.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Product } from 'src/app/models/product';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-store',
@@ -6,5 +9,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./store.component.css']
 })
 export class StoreComponent implements OnInit {
+  productList: Product[] = [];
+
+
+  constructor(private productService: ProductService, private router: Router, ) {}
+
+  ngOnInit(): void {
+    this.productService.getAllProducts().subscribe(foundProducts => {
+      console.log(foundProducts);
+      this.productList = foundProducts;
+    })
+  }
+
 
 }
