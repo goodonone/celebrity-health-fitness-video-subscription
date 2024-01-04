c4c963ceb2dd83f80d718ad797b5870332794572
code-jude <judeprashanth@outlook.com>
2024-01-04 14:16:40 -0800

    Styling to Profile Page

--- finalProject/src/app/components/profile/profile.component.ts
+++ finalProject/src/app/components/profile/profile.component.ts
@@ -14,9 +14,10 @@ export class ProfileComponent implements OnInit {
   monthOrYear!: string;
 
   tierOne = false;
-  tierTwo = false;
+  tierTwoThree = false;
   tierThree = false;
   freeTier = false;
+  firstName?: string;
 
   constructor(private userService: UserService, private router: Router, private actRoute: ActivatedRoute) { }
 
@@ -35,27 +36,32 @@ export class ProfileComponent implements OnInit {
     // console.log(UserId);
     this.userService.getUser(UserId).subscribe(user => {
       this.currentUser = user;
-      console.log(this.currentUser);
+      // console.log(this.currentUser);
       if (this.currentUser.tier === "Just Looking") {
         this.tierOne = true;
       }
-      else if (this.currentUser.tier === "Motivated") {
-        this.tierTwo = true;
+      else if (this.currentUser.tier === "Motivated" || "All In" ) {
+        this.tierTwoThree = true;
       }
-      else if (this.currentUser.tier === "All In") {
-        this.tierThree = true;
+      if (this.currentUser.paymentFrequency === "monthly") {
+        this.monthOrYear = "month";
       }
+      else if (this.currentUser.paymentFrequency === "yearly") {
+        this.monthOrYear = "year";
+      }
+  
+      if (this.currentUser.paymentFrequency === "free" || 0)
+       {
+        this.freeTier = true;
+       } 
+       const displayName  = this.currentUser.name;
+       //  console.log(displayName);
+        this.firstName = displayName?.split(' ').slice(0,1).join(' ');
+       
+      //  console.log(firstName);
     });
-
-    if (this.currentUser.paymentFrequency === "monthly") {
-      this.monthOrYear = "month";
-    }
-    else if (this.currentUser.paymentFrequency === "yearly") {
-      this.monthOrYear = "year";
-    }
-
-    if (this.currentUser.paymentFrequency === "free" || 0)
-      this.freeTier = true;
+    
+   
   }
 
   // loadUserProfile() {
