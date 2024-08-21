f4f191e72b71b4bd90fc76fdead91e0beb3062b8
code-jude <goodonone@outlook.com>
2024-08-20 17:51:04 -0700

    Styling homepage

--- celebrityFitnessProject/src/app/components/home/home.component.ts
+++ celebrityFitnessProject/src/app/components/home/home.component.ts
@@ -19,12 +19,12 @@ export class HomeComponent implements OnInit {
   
 
   images = [
-    { imageSrc: '/assets/Images/1.jpg', imageAlt: 'man 1', manName: 'Patrick 33, Motivated, North Carolina', imageText: '"I needed some desperate help and I got it and so much more!"' },
-    { imageSrc: '/assets/Images/2.jpg', imageAlt: 'man 2', manName: 'Steven 28, Motivated, New Jersey', imageText: '"What can I say, this program was a godsend 🙌!!!"' },
-    { imageSrc: '/assets/Images/3.jpg', imageAlt: 'man 3', manName: 'Alvin 40, All In, New York', imageText: '"This guy got me out of eating chips all day."' },
-    { imageSrc: '/assets/Images/4.jpg', imageAlt: 'man 4', manName: 'Shermon 32, Motivated, Ohio', imageText: '"The best money I have spent in a long time. PERIOD."' },
-    { imageSrc: '/assets/Images/5.jpg', imageAlt: 'man 5', manName: 'Jorge 38, All In, California', imageText: '"Hooooo! I got my six pack and got rid of that other six pack🤘"' },
-    { imageSrc: '/assets/Images/6.jpg', imageAlt: 'man 6', manName: 'Billy 27, Motivated, Oregon', imageText: '"Just, Start!"' }
+    { imageSrc: '/assets/Images/1.jpg', imageAlt: 'man 1', manName: 'Patrick 33, <i>Motivated</i>, North Carolina', imageText: '"I needed some desperate help and I got it and so much more!"' },
+    { imageSrc: '/assets/Images/2.jpg', imageAlt: 'man 2', manName: 'Steven 28, <i>Motivated</i>, New Jersey', imageText: '"What can I say, this program was a godsend 🙌!!!"' },
+    { imageSrc: '/assets/Images/3.jpg', imageAlt: 'man 3', manName: 'Alvin 40, <i>All In</i>, New York', imageText: '"This guy got me out of eating chips all day."' },
+    { imageSrc: '/assets/Images/4.jpg', imageAlt: 'man 4', manName: 'Shermon 32, <i>Motivated</i>, Ohio', imageText: '"The best money I have spent in a long time. PERIOD."' },
+    { imageSrc: '/assets/Images/5.jpg', imageAlt: 'man 5', manName: 'Jorge 38, <i>All In</i>, California', imageText: '"Hooooo! I got my six pack and got rid of that other six pack🤘"' },
+    { imageSrc: '/assets/Images/6.jpg', imageAlt: 'man 6', manName: 'Billy 27, <i>Motivated</i>, Oregon', imageText: '"Just, Start!"' }
   ];
 
   ngOnInit(): void {
